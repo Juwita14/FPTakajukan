8232fd46ff1aa165a3027ab34c70851da4a72250
Ammar Alifian Fahdan <kerupuksambel.2000@gmail.com>
2021-01-10 09:09:23 +0700

    Perapian struktur

--- barang/dataBarang.php
+++ barang/dataBarang.php
@@ -5,12 +5,12 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <title>Data Barang</title>
         <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-        <link rel="stylesheet" href=" bower_components/bootstrap/dist/css/bootstrap.min.css">
-        <link rel="stylesheet" href=" bower_components/font-awesome/css/font-awesome.min.css">
-        <link rel="stylesheet" href=" bower_components/Ionicons/css/ionicons.min.css">
-        <link rel="stylesheet" href="../../bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css">
-        <link rel="stylesheet" href=" dist/css/AdminLTE.min.css">
-        <link rel="stylesheet" href=" dist/css/skins/_all-skins.min.css">
+        <link rel="stylesheet" href="../assets/bower_components/bootstrap/dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="../assets/bower_components/font-awesome/css/font-awesome.min.css">
+        <link rel="stylesheet" href="../assets/bower_components/Ionicons/css/ionicons.min.css">
+        <link rel="stylesheet" href="../assets/bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css">
+        <link rel="stylesheet" href="../assets/dist/css/AdminLTE.min.css">
+        <link rel="stylesheet" href="../assets/dist/css/skins/_all-skins.min.css">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
     </head>
     <body class="hold-transition skin-blue sidebar-mini">
@@ -168,17 +168,17 @@
             <!-- ./wrapper -->
 
             <!-- jQuery 3 -->
-            <script src=" bower_components/jquery/dist/jquery.min.js"></script>
+            <script src="../assets/bower_components/jquery/dist/jquery.min.js"></script>
             <!-- Bootstrap 3.3.7 -->
-            <script src=" bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+            <script src="../assets/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
             <!-- SlimScroll -->
-            <script src=" bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+            <script src="../assets/bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
             <!-- FastClick -->
-            <script src=" bower_components/fastclick/lib/fastclick.js"></script>
+            <script src="../assets/bower_components/fastclick/lib/fastclick.js"></script>
             <!-- AdminLTE App -->
-            <script src=" dist/js/adminlte.min.js"></script>
+            <script src="../assets/dist/js/adminlte.min.js"></script>
             <!-- AdminLTE for demo purposes -->
-            <script src=" dist/js/demo.js"></script>
+            <script src="../assets/dist/js/demo.js"></script>
             <script>
             $(document).ready(function () {
                 $('.sidebar-menu').tree()
--- beranda.php
+++ beranda.php
@@ -5,18 +5,18 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <title>Takajukan</title>
         <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-        <link rel="stylesheet" href=" bower_components/bootstrap/dist/css/bootstrap.min.css">
-        <link rel="stylesheet" href=" bower_components/font-awesome/css/font-awesome.min.css">
-        <link rel="stylesheet" href=" bower_components/Ionicons/css/ionicons.min.css">
-        <link rel="stylesheet" href=" dist/css/AdminLTE.min.css">
-        <link rel="stylesheet" href=" dist/css/skins/_all-skins.min.css">
+        <link rel="stylesheet" href="assets/bower_components/bootstrap/dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="assets/bower_components/font-awesome/css/font-awesome.min.css">
+        <link rel="stylesheet" href="assets/bower_components/Ionicons/css/ionicons.min.css">
+        <link rel="stylesheet" href="assets/dist/css/AdminLTE.min.css">
+        <link rel="stylesheet" href="assets/dist/css/skins/_all-skins.min.css">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
     </head>
     <body class="hold-transition skin-blue sidebar-mini">
         <div class="wrapper">
             <header class="main-header">
                 <!-- Logo -->
-                <a href=" index2.html" class="logo">
+                <a href="index.html" class="logo">
                     <span class="logo-mini"><b>T</b></span>
                     <span class="logo-lg"><b>Takajukan</b></span>
                 </a>
